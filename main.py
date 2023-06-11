2f7ed42b5b088e94576469d84b4c6e26df6c1dbc
Ebin Paul <ebinpaul@outlook.com>
2023-06-11 22:54:46 +0100

    added author and calling functions to parse nexus file and compute ultrametric network

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+"""
+Author: Ebin Paul
+"""
 
+from utils.nexus_parser import get_distance_block
+from algorithms.floyd_warshall import get_ultrametric_network_edges
+
+filepath = "C:/Users/ebinp/Downloads/test_nexus/dolphins_binary.nex"
+
+distance_matrix, vertices = get_distance_block(filepath)
+
+ultrametric_network, ultrametric_network_delta = get_ultrametric_network_edges(
+    distance_matrix, vertices
+)
+
+print(f"{ultrametric_network = }")
