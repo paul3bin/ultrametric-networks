1418c4bc9903b91ff13a7db156ae330941509cdd
Ebin Paul <ebinpaul@outlook.com>
2023-07-15 20:07:47 +0100

    added file selection feature to the UI

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,14 +1,15 @@
 import sys
 from PyQt5.QtWidgets import (
     QApplication,
-    QWidget,
-    QLabel,
     QComboBox,
+    QHBoxLayout,
+    QLabel,
     QLineEdit,
     QPushButton,
-    QVBoxLayout,
-    QHBoxLayout,
     QTextEdit,
+    QVBoxLayout,
+    QWidget,
+    QFileDialog,
 )
 
 
@@ -18,8 +19,7 @@ class MainWindow(QWidget):
         self.initiate_ui()
 
     def initiate_ui(self):
-        # Application Name
-        # app_name_label = QLabel("UltraNet Viewer")
+        self.selected_file = None
 
         # File Information
         file_info_label = QLabel("File Information")
@@ -37,7 +37,8 @@ class MainWindow(QWidget):
         threshold_input = QLineEdit()
 
         # Run/Execute Button
-        run_button = QPushButton("Run/Execute")
+        self.run_button = QPushButton("Run/Execute")
+        self.run_button.setEnabled(False)
 
         # Choose Layout
         layout_label = QLabel("Choose Layout:")
@@ -45,13 +46,20 @@ class MainWindow(QWidget):
         layout_dropdown.addItem("Floyd-Warshall")
         layout_dropdown.addItem("UltraNet")
 
-        # Export/Save and Reset/Clear Buttons
-        export_button = QPushButton("Export/Save")
+        # Export/Save Button
+        self.export_button = QPushButton("Export/Save")
+        self.export_button.setEnabled(False)
+
+        # Reset/Clear Button
         reset_button = QPushButton("Reset/Clear")
+        reset_button.clicked.connect(self.reset_action)
+
+        # File Selection Button
+        select_file_button = QPushButton("Select File")
+        select_file_button.clicked.connect(self.open_file_dialog)
 
         # Layout
         vbox = QVBoxLayout()
-        # vbox.addWidget(app_name_label)
         vbox.addWidget(file_info_label)
         vbox.addWidget(file_info_textbox)
 
@@ -67,25 +75,41 @@ class MainWindow(QWidget):
 
         vbox.addLayout(hbox2)
 
-        vbox.addWidget(run_button)
-
         hbox3 = QHBoxLayout()
+        hbox3.addWidget(self.run_button)
         hbox3.addWidget(layout_label)
         hbox3.addWidget(layout_dropdown)
 
         vbox.addLayout(hbox3)
 
         hbox4 = QHBoxLayout()
-        hbox4.addWidget(export_button)
+        hbox4.addWidget(self.export_button)
         hbox4.addWidget(reset_button)
 
         vbox.addStretch(1)
         vbox.addLayout(hbox4)
+        vbox.addWidget(select_file_button)
 
         self.setLayout(vbox)
         self.setWindowTitle("UltraNet Viewer")
         self.setGeometry(100, 100, 400, 400)
         self.show()
+
+    def open_file_dialog(self):
+        file_dialog = QFileDialog()
+        file_path, _ = file_dialog.getOpenFileName(self, "Choose Nexus File")
+        if file_path:
+            self.selected_file = file_path
+            self.enable_buttons()
+
+    def enable_buttons(self):
+        self.run_button.setEnabled(True)
+        self.export_button.setEnabled(True)
+
+    def reset_action(self):
+        self.run_button.setEnabled(False)
+        self.export_button.setEnabled(False)
+        self.selected_file = None
 
 
 if __name__ == "__main__":
